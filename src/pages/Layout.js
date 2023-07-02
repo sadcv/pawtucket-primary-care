7484f0ec365dcbb055f19348d1f29092daa19d4d
sdacruz1 <stephanie_da_cruz@brown.edu>
2023-07-01 23:09:23 -0400

    footer basics

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -5,6 +5,7 @@ import logo from "../media/logo_with_circle.png"
 function Layout() {
   return (
     <div className="Layout">
+      {/* Header */}
       <nav style={{ color: "white", backgroundColor: "#2C7370", display: "flex", alignItems: "center" }}>
         <div style={{ display: "flex", alignItems: "center" }} >
           <img style={{ width: "3rem", height: "3rem" }} src={logo} alt="Pawtucket Primary Care logo" />
@@ -22,6 +23,53 @@ function Layout() {
       </nav>
 
       <Outlet />
+
+      {/* Footer */}
+      <div style={{ color: "white", backgroundColor: "#2C7370", display: "flex" }}>
+        <div>
+          <h3>LINKS</h3>
+          <div style={{ display: "flex" }} >
+            <div style={{ display: "flex", flexDirection: "column" }} >
+              <Link className='navLink' style={{padding: "1rem 0rem"}} to="/">Home</Link>
+              <Link className='navLink' style={{padding: "1rem 0rem"}} to="/services">Services</Link>
+              <Link className='navLink' style={{padding: "1rem 0rem"}} to="/clinician">Our Clinician</Link>
+              <Link className='navLink' style={{padding: "1rem 0rem"}} to="/contact">Contact Us</Link>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }} >
+              <Link className='navLink' style={{padding: "1rem 0rem"}} to="/hours">Hours Of Operation</Link>
+              <Link className='navLink' style={{padding: "1rem 0rem"}} to="/insurance">Insurance</Link>
+              <Link className='navLink' style={{padding: "1rem 0rem"}} to="/appointment">Book An Appointment</Link>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3>CONTACT US</h3>
+          <p>Phone: (401) 244-5900</p>
+          <p>Fax: (401) 262-5501</p>
+          <p>126 Prospect St, Suite 103, Pawtucket, RI 02860</p>
+          <p>pawtucket.primary.care@gmail.com</p>
+        </div>
+        <img style={{ width: "3rem", height: "3rem" }} src={logo} alt="Pawtucket Primary Care logo" />
+        <div>
+          <h1>Pawtucket Primary Care</h1>
+          <div style={{ display: "flex" }} >
+            <div>
+              <p>Monday:</p>
+              <p>Tuesday:</p>
+              <p>Wednesday:</p>
+              <p>Thursday:</p>
+              <p>Friday:</p>
+            </div>
+            <div>
+              <p>8:00 am - 4:30 pm</p>
+              <p>8:00 am - 4:30 pm</p>
+              <p>8:00 am - 4:30 pm</p>
+              <p>8:00 am - 4:30 pm</p>
+              <p>8:00 am - 4:30 pm</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
